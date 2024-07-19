b9211acb77a856c7b58b2e1cac23846b8903ad35
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-07-19 16:54:23 +0200

    Integrating the modal for storing books, authors and categories

--- static/js/books.js
+++ static/js/books.js
@@ -1,19 +1,72 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('submitted-status');
-    const value= form.getAttribute('value')
+/** DOMContentLoaded */
+bookForm=document.addEventListener('DOMContentLoaded', function() {
+    /** Books */
+    const bookSubmission = document.getElementById('book-submitted');
+    if (bookSubmission != null){
+        const bookSubmissionValue= bookSubmission.getAttribute('value');
+        console.log("book submission ",bookSubmissionValue)
+        /** for book submission */
+        if (bookSubmissionValue==="true") {
+            showBookConfirmationModal();
+        } else {
+            console.error('Submission failed');
+        }
+    }    
+});
 
-    if (value==="true") {
-        showConfirmationModal();
-    } else {
-        console.error('Submission failed');
+categoryForm=document.addEventListener('DOMContentLoaded', function() {
+    /** Category */
+    const categorySubmission = document.getElementById('category-submitted');
+    if(categorySubmission != null){
+        const categorySubmissionValue= categorySubmission.getAttribute('value');
+        /** for category submission */
+        if (categorySubmissionValue==="true") {
+            showCategoryConfirmationModal();
+        } else {
+            console.error('Submission failed');
+        }
     }
 });
 
-function showConfirmationModal() {
-    const confirmationModal = new bootstrap.Modal(document.getElementById('confirmationModal'));
-    confirmationModal.show();
+authorForm=document.addEventListener('DOMContentLoaded', function() {
+    /** Author */
+    const authorSubmission = document.getElementById('author-submitted');
+    if (authorSubmission != null){
+        const authorSubmissionValue= authorSubmission.getAttribute('value');
+        /** for author submission */
+        if (authorSubmissionValue==="true") {
+            showAuthorConfirmationModal();
+        } else {
+            console.error('Submission failed');
+        }
+    }
+    
+});
 
+/** Show books submission modal */
+function showBookConfirmationModal() {
+    const confirmationModal = new bootstrap.Modal(document.getElementById('bookConfirmationModal'));
+    confirmationModal.show();
     setTimeout(() => {
         confirmationModal.hide();
-    }, 5000);
+    }, 3000);
+};
+
+
+/** Show category submission modal */
+function showCategoryConfirmationModal() {
+    const confirmationModal = new bootstrap.Modal(document.getElementById('categoryConfirmationModal'));
+    confirmationModal.show();
+    setTimeout(() => {
+        confirmationModal.hide();
+    }, 3000);
+};
+
+/** Show author submission modal */
+function showAuthorConfirmationModal() {
+    const confirmationModal = new bootstrap.Modal(document.getElementById('authorConfirmationModal'));
+    confirmationModal.show();
+    setTimeout(() => {
+        confirmationModal.hide();
+    }, 3000);
 };
